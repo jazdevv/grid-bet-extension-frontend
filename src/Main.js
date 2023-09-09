0f5fd6ce941be87916bef9f7e2c9e59e101126c0
Jazthedev <joelpepa2@gmail.com>
2023-09-09 12:34:12 +0200

    update: first render

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,14 @@ import Login from './Components/Login';
 import { useState } from 'react';
 import App from "./Components/App"
 function Main() {
+  const [firstRender,setFirstRender] = useState(true);
   const [jwtBet,setJwtBet] = useState(undefined);
   const jwtbetLocal = localStorage.getItem("jwtbet");
-  if(jwtBet == undefined ){
+  if(jwtBet == undefined && firstRender == true){
     setJwtBet(jwtbetLocal);
+    setFirstRender(false);
   }
+  console.log("jwtbet",jwtBet);
   return (
     <div className="p-4 w-96 custom-min-h-96 bg-gray-900">
       {jwtBet ? <App jwtbet={jwtBet} /> : <Login setJwtBet={setJwtBet}/>}
